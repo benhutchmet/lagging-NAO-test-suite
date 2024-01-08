16bae35dbd8e12d0d567aca7d765c7a02321ae1c
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-08 14:51:25 +0000

    now test new sequence of submit

--- calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
+++ calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=ben-array-sel-season-test
+#SBATCH --job-name=ben-array-sel-season-test-years
 #SBATCH --partition=test
-#SBATCH -o /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test/%j.out
-#SBATCH -e /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test/%j.err
-#SBATCH --time=30:00
+#SBATCH -o /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/%j.out
+#SBATCH -e /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/%j.err
+#SBATCH --time=10:00
 #SBATCH --array=1960-1965
 
 # FIXME: Replace test with short-serial once tested
@@ -62,67 +62,7 @@ echo "Experiment is: $experiment"
 module load jaspy
 
 # Set up the process script
-process_script=/home/users/benhutch/lagging-NAO-test-suite/calc_anoms_suite/process_scripts/multi-model.sel-region-forecast-range-season.bash
-
-# Declare an empty associative array
-declare -A nens_extractor
-
-# Extract the models list using a case statement
-case $variable in
-"psl")
-    models=$models
-
-    # Loop over and copy each key-value pair from the psl_models_nens
-    for key in "${!psl_models_nens[@]}"; do
-        nens_extractor[$key]=${psl_models_nens[$key]}
-    done
-    ;;
-"sfcWind")
-    models=$sfcWind_models
-
-    # Loop over and copy each key-value pair from the sfcWind_models_nens
-    for key in "${!sfcWind_models_nens[@]}"; do
-        nens_extractor[$key]=${sfcWind_models_nens[$key]}
-    done
-    ;;
-"rsds")
-    models=$rsds_models
-
-    # Loop over and copy each key-value pair from the rsds_models_nens
-    for key in "${!rsds_models_nens[@]}"; do
-        nens_extractor[$key]=${rsds_models_nens[$key]}
-    done
-    ;;
-"tas")
-    models=$tas_models
-
-    # loop over and copy each key-value pair from the tas_models_nens
-    for key in "${!tas_models_nens[@]}"; do
-        nens_extractor[$key]=${tas_models_nens[$key]}
-    done
-    ;;
-"tos")
-    models=$tos_models
-
-    # Loop over and copy each key-value pair from the tos_models_nens
-    for key in "${!tos_models_nens[@]}"; do
-        nens_extractor[$key]=${tos_models_nens[$key]}
-    done
-    ;;
-*)
-    echo "ERROR: variable not recognized: $variable"
-    exit 1
-    ;;
-esac
-
-# Echo the models
-echo "Models are: $models"
-
-# Echo the values of the nens_extractor
-echo "Values of nens_extractor are: ${nens_extractor[@]}"
-
-# Echo the keys of the nens_extractor
-echo "Keys of nens_extractor are: ${!nens_extractor[@]}"
+member_batch_script="/home/users/benhutch/lagging-NAO-test-suite/calc_anoms_suite/submit_scripts/test-sel-season-member-array.bash"
 
 #FIXME: NENS extractor not working, but we use this in a different mode
 # If model is all
@@ -160,34 +100,16 @@ if [ $model == "all" ]; then
 fi
 
 # In the case of individual models
-echo "Processing single model: $model"
+echo "Submitting single model: $model"
 
 # Echo the year which we are processing
-echo "Processing year: ${SLURM_ARRAY_TASK_ID}"
-
-# Declare the nen_extractor
-declare -p nens_extractor
+echo "Submitting year: ${SLURM_ARRAY_TASK_ID}"
 
 # Echo the model
 echo "Model is: $model"
 
-# Extract the number of ensemble members for a single model
-nens=${nens_extractor[$model]}
-
-# Echo the number of ensemble members
-echo "Number of ensemble members: $nens"
-
-# Loop over the ensemble members
-for run in $(seq 1 $nens); do
-
-    # Echo the ensemble member
-    echo "Processing run: $run"
-
-    # # Run the process script as an array job
-    bash $process_script ${model} ${SLURM_ARRAY_TASK_ID} ${run} ${variable} \
-        ${season} ${experiment}
-
-done
+# Run the batch script for submitting the ensemble member
+sbatch ${member_batch_script} ${model} ${variable} ${season} ${experiment} ${SLURM_ARRAY_TASK_ID}
 
 # End of script
-echo "Finished processing ${model} ${variable} ${season} ${experiment} ${start_year} ${end_year}"
+echo "Finished processing ${model} ${variable} ${season} ${experiment} ${SLURM_ARRAY_TASK_ID}"
