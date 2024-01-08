07f824f16c7be277a4877b4ba73c25ae53923a70
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-08 14:26:32 +0000

    changes to model mean state file

--- process_scripts/multi-model.calc-anoms-model-mean-state.bash
+++ process_scripts/multi-model.calc-anoms-model-mean-state.bash
@@ -4,13 +4,13 @@
 #
 # Script for calculating the model mean state for a given model.
 #
-# For example: calculate-model-mean-states.bash HadGEM3-GC31-MM psl north-atlantic 2-5 DJF
+# For example: calculate-model-mean-states.bash HadGEM3-GC31-MM psl DJFM
 
 # Set the usage message
-USAGE_MESSAGE="Usage: multi-model.calc-anoms-model-mean-state.bash <model> <variable> <region> <forecast-range> <season>"
+USAGE_MESSAGE="Usage: multi-model.calc-anoms-model-mean-state.bash <model> <variable> <season>"
 
 # Check that the correct number of arguments have been passed
-if [ $# -ne 5 ]; then
+if [ $# -ne 3 ]; then
     echo "$USAGE_MESSAGE"
     exit 1
 fi
@@ -18,24 +18,31 @@ fi
 # Extract the model, variable, region, forecast range and season
 model=$1
 variable=$2
-region=$3
-forecast_range=$4
-season=$5
+season=$3
 
 # Load cdo
 module load jaspy
 
 # Base directory
-base_dir="/work/scratch-nopw2/benhutch/${variable}/${model}/${region}/years_${forecast_range}/${season}/outputs"
+base_dir="/work/scratch-nopw2/benhutch/${variable}/${model}/global/all_forecast_years/${season}/outputs"
+
+# Check that the base directory exists
+if [ ! -d $base_dir ]; then
+    echo "ERROR: base directory does not exist"
+    exit 1
+fi
 
 # Function for processing files
 process_files() {
     init_scheme=$1
-    files_path="$base_dir/years-${forecast_range}-${season}-${region}-${variable}_Amon_${model}_dcppA-hindcast_s????-r*${init_scheme}*.nc"
+    files_path="$base_dir/all-years-${season}-global-${variable}_Amon_${model}_dcppA-hindcast_s????-r*i${init_scheme}*.nc"
+
+    # Test this path
+    # all-years-DJFM-global-psl_Amon_HadGEM3-GC31-MM_dcppA-hindcast_s????-r*i*.nc"
 
     # Echo the files to be processed
     echo "Calculating model mean state for: $files_path"
-    echo "Calculating model mean state for model $model, variable $variable, region $region, forecast range $forecast_range, season $season and init scheme $init_scheme"
+    echo "Calculating model mean state for model $model, variable $variable, region global, all forecast years, season $season and init scheme $init_scheme"
 
     # Set up the file name for the model mean state
     temp_model_mean_state="$base_dir/tmp/model_mean_state_${init_scheme}.nc"
@@ -70,7 +77,6 @@ case $model in
     "EC-Earth3")
         process_files "i1"
         process_files "i2"
-        process_files "i4"
         ;;
     *)
         # For all other models, use a wildcard for init_scheme
@@ -81,7 +87,8 @@ esac
 # Clean up temporary files
 rm -f ${base_dir}/tmp/temp-*.nc
 
-echo "Model mean states have been calculated for $model $variable $region $forecast_range $season and saved in $base_dir/tmp"
+echo "Model mean states have been calculated for $model $variable $season and saved in $base_dir/tmp"
+echo "Model mean state output file: ${temp_model_mean_state}"    
 
 # End of script
 exit 0
