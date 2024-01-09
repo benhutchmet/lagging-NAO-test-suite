3958adf5726962c0c559dd476420791bb3cd71c5
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-09 15:00:03 +0000

    now test mergetime script

--- calc_anoms_suite/process_scripts/multi-model.mergetime.bash
+++ calc_anoms_suite/process_scripts/multi-model.mergetime.bash
@@ -8,10 +8,10 @@
 # For example: multi-model.mergetime.bash HadGEM3-GC31-MM psl north-atlantic 2-5 DJF 1 1 92500
 #
 
-USAGE_MESSAGE="Usage: multi-model.mergetime.bash <model> <variable> <region> <forecast-range> <season> <run> <init_scheme> <pressure-level>"
+USAGE_MESSAGE="Usage: multi-model.mergetime.bash <model> <variable> <season> <run> <init_scheme>"
 
 # check that the correct number of arguments have been passed
-if [ $# -ne 8 ]; then
+if [ $# -ne 5 ]; then
     echo "$USAGE_MESSAGE"
     exit 1
 fi
@@ -19,24 +19,22 @@ fi
 # extract the model, variable, region, forecast range and season
 model=$1
 variable=$2
-region=$3
-forecast_range=$4
-season=$5
-
-# extract the run and init_scheme
-run=$6
-init_scheme=$7
-pressure_level=$8
+season=$3
+run=$4
+init_scheme=$5
 
 # make sure that cdo is loaded
 module load jaspy
 
 if [ "$variable" == "ua" ] || [ "$variable" == "va" ]; then
     # anoms directory from which to extract the files
-    base_dir="/work/scratch-nopw2/benhutch/$variable/$model/$region/years_${forecast_range}/$season/plev_${pressure_level}/outputs/anoms"
+    # base_dir="/work/scratch-nopw2/benhutch/$variable/$model/$region/years_${forecast_range}/$season/plev_${pressure_level}/outputs/anoms"
+    echo "ERROR: pressure level not set up for ua and va"
+    exit 1
 else
     # anoms directory from which to extract the files
-    base_dir="/work/scratch-nopw2/benhutch/$variable/$model/$region/years_${forecast_range}/$season/outputs/anoms"
+    # example dir: /work/scratch-nopw2/benhutch/psl/BCC-CSM2-MR/global/all_forecast_years/DJFM/outputs/anoms
+    base_dir="/work/scratch-nopw2/benhutch/$variable/$model/global/all_forecast_years/$season/outputs/anoms"
 fi    
 
 # If the variable is ua or va
@@ -47,12 +45,20 @@ if [ "$variable" == "ua" ] || [ "$variable" == "va" ]; then
     files_pattern="mean-years-${forecast_range}-${season}-${region}-plev-${variable}_?mon_${model}_dcppA-hindcast_s????-r${run}i${init_scheme}*-anoms.nc"
 else
     # file pattern of the anoms files
-    files_pattern="mean-years-${forecast_range}-${season}-${region}-${variable}_?mon_${model}_dcppA-hindcast_s????-r${run}i${init_scheme}*-anoms.nc"
+    # EXample: all-years-DJFM-global-psl_Amon_BCC-CSM2-MR_dcppA-hindcast_s1975-r8i1p1f1_gn_197501-198412-anoms.nc
+    files_pattern="all-years-${season}-global-${variable}_?mon_${model}_dcppA-hindcast_s????-r${run}i${init_scheme}*-anoms.nc"
 fi
 
 # set up the files
 # combine the base directory and the file pattern
 files="${base_dir}/${files_pattern}"
+
+# If there are no files
+# Then exit with an error
+if [ ! -f $files ]; then
+    echo "ERROR: no files found: $files"
+    exit 1
+fi
 
 # echo the files to be merged
 echo "Files to be merged: $files"
@@ -65,17 +71,19 @@ fi
 
 if [ "$variable" == "ua" ] || [ "$variable" == "va" ]; then
     # Set the output directory with the pressure level
-    OUTPUT_DIR="/home/users/benhutch/skill-maps-processed-data/${variable}/${model}/${region}/years_${forecast_range}/${season}/plev_${pressure_level}/outputs/mergetime"
+    # OUTPUT_DIR="/home/users/benhutch/skill-maps-processed-data/${variable}/${model}/${region}/years_${forecast_range}/${season}/plev_${pressure_level}/outputs/mergetime"
+    echo "ERROR: pressure level not set up for ua and va"
+    exit 1
 else
     # set the output directory
     # send to the home directory
-    OUTPUT_DIR="/home/users/benhutch/skill-maps-processed-data/${variable}/${model}/${region}/years_${forecast_range}/${season}/outputs/mergetime"
+    OUTPUT_DIR="/gws/nopw/j04/canari/users/benhutch/skill-maps-processed-data/${variable}/${model}/global/all_forecast_years/${season}/outputs/mergetime"
 fi
 
 mkdir -p $OUTPUT_DIR
 
 # set the output file
-mergetime_fname="mergetime_${model}_${variable}_${region}_${forecast_range}_${season}-r${run}i${init_scheme}.nc"
+mergetime_fname="mergetime_${model}_${variable}_global_all_forecast_years_${season}-r${run}i${init_scheme}.nc"
 OUTPUT_FILE=${OUTPUT_DIR}/${mergetime_fname}
 
 # echo the output file
