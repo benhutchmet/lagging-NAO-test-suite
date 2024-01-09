ad5b0860d808583f1f794eb583a2151b77e528b6
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-09 15:41:01 +0000

    edit throttle and run again

--- calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
+++ calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
@@ -5,9 +5,7 @@
 #SBATCH -o /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/sel-season-test-years-%A_%a.out
 #SBATCH -e /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/sel-season-test-years-%A_%a.err
 #SBATCH --time=60:00
-#SBATCH --array=1960-2018%4
-
-# FIXME: Replace 1960-1965 with 1960-2018 once tested
+#SBATCH --array=1960-2018%20
 
 # Form the path for the logs folder and make sure it exists
 logs_dir="/gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years"
