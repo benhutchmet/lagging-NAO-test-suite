121ec1f56d79f7fb59f7ad6f34b935b0e9368ff6
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-09 13:50:20 +0000

    increase the time limit

--- calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
+++ calc_anoms_suite/submit_scripts/test-sel-region-array-script.bash
@@ -4,7 +4,7 @@
 #SBATCH --partition=short-serial
 #SBATCH -o /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/%j.out
 #SBATCH -e /gws/nopw/j04/canari/users/benhutch/batch_logs/ben-array-sel-season-test-years/%j.err
-#SBATCH --time=30:00
+#SBATCH --time=60:00
 #SBATCH --array=1960-1970%2
 
 # FIXME: Replace 1960-1965 with 1960-2018 once tested
