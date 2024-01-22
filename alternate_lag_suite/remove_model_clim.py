6b9d0a62b1dee4e7ba6540a9c68ac9630bf8ba1b
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-22 13:00:02 +0000

    new script being developed

---
+++ alternate_lag_suite/remove_model_clim.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+
+"""
+remove_model_clim.py
+====================
+
+A script which removes the model climatology, for a specific season and range
+of forecast years, from the model data.
+
+Author: Ben Hutchins
+Date: January 2024
+
+Usage:
+
+    $ python remove_model_clim.py <model> 
+    
+    E.g. $ python remove_model_clim.py DJF 1981 2010"""
