34c65625bf477384e6445d9e9a4c8398a27be832
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-22 14:41:56 +0000

    fix indentation

--- alternate_lag_suite/remove_model_clim.py
+++ alternate_lag_suite/remove_model_clim.py
@@ -75,115 +75,118 @@ def check_files_exist(model: str,
                         end_year: int,
                         region: str,
                         forecast_range: str = "all_forecast_years",
-                        base_dir: str = "/work/scratch-nopw2/benhutch/") -> bool:
-        """
-        Checks whether all of the files exist.
-    
-        Parameters:
-        ----------
-    
-        model : str
-            The name of the model to be used.
-            E.g. 'BCC-CSM2-MR'
-    
-        variable : str
-            The name of the variable to be used.
-            E.g. 'psl'
-    
-        season : str
-            The season to be used.
-            E.g. 'DJFM'
-    
-        start_year : int
-            The start year of the forecast range.
-            E.g. 1961
-    
-        end_year : int
-            The end year of the forecast range.
-            E.g. 2014
-    
-        region : str
-            The region to be used.
-            E.g. 'global'
-    
-        forecast_range : str
-            The forecast range to be used.
-            E.g. '2-9'
-    
-        Returns:
-    
-        files:
-            A list containing the paths to all of the files for the given
-            parameters.
-        """
-    
-        # Initialise a list to store the paths to the files
-        files = []
+                        base_dir: str = "/work/scratch-nopw2/benhutch/") -> list:
+    """
+    Checks whether all of the files exist.
 
-        # Form the path
-        path = os.path.join(base_dir, variable, model, region, forecast_range,
-                       season, "outputs")
-        
-        # Assert that the path exists
-        assert os.path.exists(path), "The path does not exist."
+    Parameters:
+    ----------
 
-        # Find the files for the start year
-        start_year_pattern = f"{path}/*s{start_year}*"
+    model : str
+        The name of the model to be used.
+        E.g. 'BCC-CSM2-MR'
+
+    variable : str
+        The name of the variable to be used.
+        E.g. 'psl'
+
+    season : str
+        The season to be used.
+        E.g. 'DJFM'
+
+    start_year : int
+        The start year of the forecast range.
+        E.g. 1961
+
+    end_year : int
+        The end year of the forecast range.
+        E.g. 2014
+
+    region : str
+        The region to be used.
+        E.g. 'global'
+
+    forecast_range : str
+        The forecast range to be used.
+        E.g. '2-9'
+
+    Returns:
+
+    files:
+        A list containing the paths to all of the files for the given
+        parameters.
+    """
+
+    # Initialise a list to store the paths to the files
+    files = []
+
+    # Form the path
+    path = os.path.join(base_dir, variable, model, region, forecast_range,
+                    season, "outputs")
+    
+    # Assert that the path exists
+    assert os.path.exists(path), "The path does not exist."
+
+    # Find the files for the start year
+    start_year_pattern = f"{path}/*s{start_year}*"
+
+    # Find the len of the files which match the pattern
+    start_year_len = len(glob.glob(start_year_pattern))
+
+    # Find the number of uniqu "r*i?" values
+    # in
+    # all-years-DJFM-global-psl_Amon_HadGEM3-GC31-MM_dcppA-hindcast_s2018-r8i1_gn_201811-202903.nc
+    no_ens = len(np.unique([file.split("_")[6] for file in glob.glob(start_year_pattern)]))
+
+    # Print the number of ensemble members
+    print(f"Number of ensemble members: {no_ens}")
+
+    # Create a list of the unique combinations of "r*i?"
+    ens_list = np.unique([file.split("_")[6] for file in glob.glob(start_year_pattern)])
+
+    # Print the list
+    print(f"Ensemble members: {ens_list}")
+
+    # Split the ens_list by "-"
+    ens_list = [ens.split("-")[1] for ens in ens_list]
+
+    # Print the list
+    print(f"Ensemble members: {ens_list}")
+
+    # Loop over the forecast years
+    for year in range(start_year, end_year + 1):
+        # Find the files for the year
+        year_pattern = f"{path}/*s{year}*"
 
         # Find the len of the files which match the pattern
-        start_year_len = len(glob.glob(start_year_pattern))
+        year_len = len(glob.glob(year_pattern))
 
-        # Find the number of uniqu "r*i?" values
-        # in
-        # all-years-DJFM-global-psl_Amon_HadGEM3-GC31-MM_dcppA-hindcast_s2018-r8i1_gn_201811-202903.nc
-        no_ens = len(np.unique([file.split("_")[6] for file in glob.glob(start_year_pattern)]))
+        # Print
+        print(f"Number of files for s{year}: {year_len}")
 
-        # Print the number of ensemble members
-        print(f"Number of ensemble members: {no_ens}")
+        # Assert that the number of files is the same as the number of
+        # ensemble members
+        assert year_len == no_ens, "The number of files does not match the number of ensemble members."
 
-        # Create a list of the unique combinations of "r*i?"
-        ens_list = np.unique([file.split("_")[6] for file in glob.glob(start_year_pattern)])
+        # Loop over the ensemble members
+        for ens in ens_list:
+            # Find the file for the ensemble member
+            file = f"{path}/*s{year}*{ens}*"
 
-        # Print the list
-        print(f"Ensemble members: {ens_list}")
+            # Find the full path
+            file_path = glob.glob(file)
 
-        # Split the ens_list by "-"
-        ens_list = [ens.split("-")[1] for ens in ens_list]
+            # Assert that the file exists
+            assert len(file_path) == 1, "The file does not exist."
 
-        # Print the list
-        print(f"Ensemble members: {ens_list}")
+            # Append the file to the list
+            files.append(file_path[0])
 
-        # Loop over the forecast years
-        for year in range(start_year, end_year + 1):
-            # Find the files for the year
-            year_pattern = f"{path}/*s{year}*"
+    # Return the list
+    return files
 
-            # Find the len of the files which match the pattern
-            year_len = len(glob.glob(year_pattern))
 
-            # Print
-            print(f"Number of files for s{year}: {year_len}")
 
-            # Assert that the number of files is the same as the number of
-            # ensemble members
-            assert year_len == no_ens, "The number of files does not match the number of ensemble members."
-
-            # Loop over the ensemble members
-            for ens in ens_list:
-                # Find the file for the ensemble member
-                file = f"{path}/*s{year}*{ens}*"
-
-                # Find the full path
-                file_path = glob.glob(file)
-
-                # Assert that the file exists
-                assert len(file_path) == 1, "The file does not exist."
-
-                # Append the file to the list
-                files.append(file_path[0])
-
-        # Return the list
-        return files
 
 # Define a main function
 def main():
