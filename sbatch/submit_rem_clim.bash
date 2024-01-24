33834fc6f075baad3b23a478e5a1d3fbeefedbbb
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-24 13:58:47 +0000

    run this script now

--- sbatch/submit_rem_clim.bash
+++ sbatch/submit_rem_clim.bash
@@ -2,21 +2,21 @@
 #SBATCH --partition=high-mem
 #SBATCH --mem=50000
 #SBATCH --time=60:00
+#SBATCH --array=1-12
 
 # Check the number of command line arguments
-if [ "$#" -ne 7 ]; then
-    echo "Usage: ${model} ${variable} ${season} ${start_year} ${end_year} ${region} ${forecast_range}"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: ${variable} ${season} ${start_year} ${end_year} ${region} ${forecast_range}"
     exit
 fi
 
 # Extract the arguments
-model=$1
-variable=$2
-season=$3
-start_year=$4
-end_year=$5
-region=$6
-forecast_range=$7
+variable=$1
+season=$2
+start_year=$3
+end_year=$4
+region=$5
+forecast_range=$6
 
 module load jaspy
-python /home/users/benhutch/lagging-NAO-test-suite/alternate_lag_suite/remove_model_clim.py $model $variable $season $start_year $end_year $region $forecast_range
+python /home/users/benhutch/lagging-NAO-test-suite/alternate_lag_suite/remove_model_clim.py ${SLURM_ARRAY_TASK_ID} $variable $season $start_year $end_year $region $forecast_range
