c8d4b70ed06aaf06a6f7b1ba9ff9489722d0ad40
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-24 12:55:31 +0000

    cylc not running, submit differently

---
+++ sbatch/submit_rem_clim.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=high-mem
+#SBATCH --mem=50000
+#SBATCH --time=60:00
+
+# Check the number of command line arguments
+if [ "$#" -ne 7 ]; then
+    echo "Usage: ${model} ${variable} ${season} ${start_year} ${end_year} ${region} ${forecast_range}"
+    exit
+fi
+
+# Extract the arguments
+model=$1
+variable=$2
+season=$3
+start_year=$4
+end_year=$5
+region=$6
+forecast_range=$7
+
+module load jaspy
+python /home/users/benhutch/lagging-NAO-test-suite/alternate_lag_suite/remove_model_clim.py $model $variable $season $start_year $end_year $region $forecast_range
