85950be238eb8e0bb588aaf933cbcfe2dfc86c47
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-02-23 16:32:41 +0000

    testing new nao matching processing

--- alternate_lag_suite/alternate_lag_functions.py
+++ alternate_lag_suite/alternate_lag_functions.py
@@ -66,6 +66,7 @@ Outputs:
 # Import local modules
 import sys
 import os
+import glob
 import argparse
 from time import time
 import fnmatch
@@ -177,9 +178,24 @@ def load_data(
     ]:
         # Set the number of forecast years
         no_forecast_years = 9
-    else:
+    elif forecast_range in ["1", "2", "3", "4", "5", "6"] and season not in [
+        "DJFM",
+        "DJF",
+        "ONDJFM",
+        "NDJFM",
+    ]:
+        # Set the number of forecast years
+        no_forecast_years = 8
+    elif forecast_range in ["1", "2", "3", "4", "5", "6"] and season in [
+        "DJFM",
+        "DJF",
+        "ONDJFM",
+        "NDJFM",
+    ]:
         # Set the number of forecast years
         no_forecast_years = 9
+    else:
+        print("Forecast range not recognised")
 
     # extract the forecast range
     if "-" in forecast_range:
@@ -669,11 +685,11 @@ def alternate_lag(
 
 # Define a function to calculate the NAO index for the period specified
 # And optionally plot this
-def calculate_NAO_index(
+def calculate_nao_index(
     season: str,
-    start_year: int,
-    end_year: int,
     forecast_range: str,
+    start_year: int = 1961,
+    end_year: int = 2014,
     variable: str = "psl",
     models_list: list = dicts.models,
     plot: bool = False,
@@ -695,14 +711,14 @@ def calculate_NAO_index(
     season: str
         The season to calculate the NAO index for.
 
+    forecast_range: str
+        The forecast range to calculate the NAO index for.
+
     start_year: int
         The start year of the period to calculate the NAO index for.
 
     end_year: int
         The end year of the period to calculate the NAO index for.
-
-    forecast_range: str
-        The forecast range to calculate the NAO index for.
 
     models_list: list
         The list of models to calculate the NAO index for.
@@ -759,35 +775,35 @@ def calculate_NAO_index(
     # Set up the first and last years according to the forecast range
     if forecast_range == "2-9":
         # Set up the raw first and last years
-        first_year = int(start_year) + 5
-        last_year = int(end_year) + 5
+        first_year_align = int(start_year) + 5
+        last_year_align = int(end_year) + 5
     elif forecast_range == "2-5":
         # Set up the raw first and last years
-        first_year = int(start_year) + 3
-        last_year = int(end_year) + 3
+        first_year_align = int(start_year) + 3
+        last_year_align = int(end_year) + 3
     elif forecast_range == "2-3":
         # Set up the raw first and last years
-        first_year = int(start_year) + 2
-        last_year = int(end_year) + 2
+        first_year_align = int(start_year) + 2
+        last_year_align = int(end_year) + 2
     elif forecast_range == "2":
         # Set up the raw first and last years
-        first_year = int(start_year) + 1
-        last_year = int(end_year) + 1
+        first_year_align = int(start_year) + 1
+        last_year_align = int(end_year) + 1
     elif forecast_range == "1":
         # Set up the raw first and last years
-        first_year = int(start_year)
-        last_year = int(end_year)
+        first_year_align = int(start_year)
+        last_year_align = int(end_year)
     else:
         print("Forecast range not recognised")
 
     # If the season is not in the winter - i.e. data has not been shifted
     if season not in ["DJFM", "DJF", "ONDJFM", "NDJFM"]:
         # Add 1 to the first and last years
-        first_year = first_year + 1
-        last_year = last_year + 1
+        first_year_align = first_year_align + 1
+        last_year_align = last_year_align + 1
 
     # Set up the common years
-    common_years = np.arange(first_year, last_year + 1)
+    common_years = np.arange(first_year_align, last_year_align + 1)
 
     # First calculate the observed MSLP anomaly fields
     obs_psl_anomaly = funcs.read_obs(
@@ -795,14 +811,14 @@ def calculate_NAO_index(
         region=region,
         forecast_range=forecast_range,
         season=season,
-        observation_path=nms_funcs.find_obs_path(match_var=variable),
+        observations_path=nms_funcs.find_obs_path(match_var=variable),
         start_year=1960,
         end_year=2023,
     )
 
     # Constrain the obs_psl_anomaly to the common years
     obs_psl_anomaly = obs_psl_anomaly.sel(
-        time=slice(f"{first_year}-01-01", f"{last_year}-12-31")
+        time=slice(f"{first_year_align}-01-01", f"{last_year_align}-12-31")
     )
 
     # Extract the data for the south grid
@@ -834,6 +850,87 @@ def calculate_NAO_index(
 
     # Print the shape of the obs_nao_index
     print("Shape of obs_nao_index: ", obs_nao_index.shape)
+
+    # Set up an empty list for all of the ensemble member paths
+    variant_labels_models = {}
+
+    # Loop over the models
+    for model in models_list:
+        # Set up the model path
+        model_path = os.path.join(
+            base_dir, variable, model, region, forecast_range, season, "outputs"
+        )
+
+        # Assert that the model path exists
+        assert os.path.isdir(model_path), f"{model_path} does not exist."
+
+        # Find the anoms files
+        file_path_list = [
+            os.path.join(model_path, file)
+            for file in os.listdir(model_path)
+            if file.endswith(f"_start_{start_year}_end_{end_year}_anoms.nc")
+        ]
+
+        # Extract the final string after th "/"
+        file_path_list_split = [file.split("/")[-1] for file in file_path_list]
+
+        # Split the final string by "_"
+        file_path_list_split = [file.split("_")[4] for file in file_path_list_split]
+
+        # Split this by "-"
+        file_path_list_split = [file.split("-")[1] for file in file_path_list_split]
+
+        # Find the unique combinations of r and i
+        unique_members_model = np.unique(file_path_list_split)
+
+        # Print the length of the unique members
+        print(f"Length of unique members for {model}: ", len(unique_members_model))
+
+        # Append the unique members to the dictionary
+        variant_labels_models[model] = unique_members_model
+
+    # Set up a list to store the member files
+    member_files = []
+
+    # Loop over the models
+    for model in models_list:
+        # Loop over the variant label
+        for variant_label in variant_labels_models[model]:
+            # Set up the variant label files
+            variant_label_files = []
+
+            # Loop over the years
+            for year in range(start_year, end_year + 1):
+                # Find the file for the given model,
+                # year and member in the anoms_files list
+                file_path = f"{base_dir}/{variable}/{model}/{region}/{forecast_range}/{season}/outputs/*s{year}-{variant_label}*years_{forecast_range}_start_{start_year}_end_{end_year}_anoms.nc"
+
+                # Find the file for the given model,
+                # year and member in the anoms_files list
+                file = glob.glob(file_path)
+
+                # Assert that there is only one file
+                if len(file) != 1:
+                    print("More than one file found for: ", file_path)
+                    print("Files found: ", file[0].split("/")[-1])
+
+                # Append the file to the variant label files
+                variant_label_files.append(file[0])
+
+            # Append the variant label files to the member files
+            member_files.append(variant_label_files)
+
+    # Assert that member_files is a list of lists
+    assert isinstance(member_files, list), "member_files is not a list"
+
+    # Assert that member_files is a list of lists
+    assert isinstance(member_files[0], list), "member_files is not a list of lists"
+
+    # Return the member files
+    return member_files
+
+
+# Write a function for preprocessing the data
 
 
 # Define the main function
