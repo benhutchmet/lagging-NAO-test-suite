793a83f2a3745cada20358dafd513a2ecb3512ab
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-02-27 17:50:36 +0000

    suite updated

--- sbatch/submit_rem_clim.bash
+++ sbatch/submit_rem_clim.bash
@@ -3,15 +3,14 @@
 #SBATCH --partition=high-mem
 #SBATCH --mem=50000
 #SBATCH --time=1000:00
-#SBATCH --array=8
 #SBATCH -o /home/users/benhutch/lagging-NAO-test-suite/logs/rem-clim-%A_%a.out
 #SBATCH -e /home/users/benhutch/lagging-NAO-test-suite/logs/rem-clim-%A_%a.err
 #SBATCH --mail-user=benwhutchins25@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL,ARRAY_TASKS
 
 # Check the number of command line arguments
-if [ "$#" -ne 6 ]; then
-    echo "Usage: ${variable} ${season} ${start_year} ${end_year} ${region} ${forecast_range}"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: ${model} ${variable} ${season} ${start_year} ${end_year} ${region} ${forecast_range}"
     exit
 fi
 
@@ -19,12 +18,13 @@ fi
 process_script="/home/users/benhutch/lagging-NAO-test-suite/alternate_lag_suite/remove_model_clim.py"
 
 # Extract the arguments
-variable=$1
-season=$2
-start_year=$3
-end_year=$4
-region=$5
-forecast_range=$6
+model=$1
+variable=$2
+season=$3
+start_year=$4
+end_year=$5
+region=$6
+forecast_range=$7
 
 module load jaspy
-python $process_script ${SLURM_ARRAY_TASK_ID} $variable $season $start_year $end_year $region $forecast_range
+python $process_script $model $variable $season $start_year $end_year $region $forecast_range
